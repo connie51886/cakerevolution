56efdb4eb873c1477e81e588f205bf533276a5bd
Connie Lin <connielin@Connies-Air.lan>
2023-05-10 01:38:06 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const items = [
+    { name: 'cupcake1', category: 'cupcake',  img: './images/cupcake2.png' },
+    { name: 'cheesecake1', category: 'cheesecake', img: './images/cheesecake2.png'},
+    { name: 'birthdaycake1', category: 'birthday cake', img: './images/birthday2.png'},
+    { name: 'Hungry', category: 'none', img: './images/hungry2.png'},
+  ];
+
+  
+  const [menuItems, setMenuItems] = useState(
+    items.filter(item => item.category === 'none')
+  );
+  
+  const [category, setCategory] = useState('none');
+
+  function filterItems(category) {
+    const newItems = items.filter(item => item.category === category);
+    setMenuItems(newItems);
+    setCategory(category);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="marquee-container" >
+      
+            <span class="marquee-text" >
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+             <a href="https://www.youtube.com/watch?v=9Et-chfMdXA&ab_channel=BingeSociety" target="blank"> Qu'ils mangent de la brioche ⚜️ </a>
+            </span>
+
+    
+      </div>
+      
+
+    <section class="mainContent">
+      <section class="chooseHere">
+        <h1>"Let them eat cake!"</h1>
+
+        <button class="border-button" onClick={() => filterItems('cupcake')}>I want cupcake</button>
+        <button class="border-button" onClick={() => filterItems('cheesecake')}>I want cheesecake</button>
+        <button class="border-button" onClick={() => filterItems('birthday cake')}>I want birthday cake</button>
+      </section>
+      
+      <ul>
+        {menuItems.map((item, index) => (
+          <img key={index} src={item.img} alt={item.name}/>
+        ))}
+      </ul>
+    </section>
     </div>
   );
 }
+
 
 export default App;
