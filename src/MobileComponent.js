3284ae133fe04f2f2ffedf901dbfa7adb38764bd
Connie Lin <connielin@Connies-Air.lan>
2023-05-10 21:09:10 +0200

    mobile css file

--- src/MobileComponent.js
+++ src/MobileComponent.js
@@ -1,41 +1,17 @@
 import React from 'react';
-
+import './mobile.css';
 
 const MobileComponent = () => {
-    const componentStyle = {
-        backgroundImage: 'url(./images/mobilecake.png)',
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center',
-
-
-        height: '100%'
-      };
     
-    
-  const headingStyle = {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
-    margin: '0'
-    
-  };
 
     return (
-      <div style={{height:'100%'}}>
-        <div style = {{height:'100%'}}>
-      
-          <div className="mobile-component" style={componentStyle}>
+          <div class="background">
 
-              <h2 style = {headingStyle}>Pls come back on desktop!</h2>
+              <h2>Pls come back on desktop!</h2>
 
           </div>
 
-      </div>
-      </div>
+     
     );
   };
   
